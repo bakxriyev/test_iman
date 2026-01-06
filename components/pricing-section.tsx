c28335247c5c9ac65b88d8c053e1db98cf9a1379
bakxriyev <kamronbekbahriyev18@gmail.com>
2026-01-07 01:38:41 +0500

    dssd

--- components/pricing-section.tsx
+++ components/pricing-section.tsx
@@ -132,6 +132,26 @@ const PricingSection = ({ onOpenForm }: { onOpenForm?: () => void }) => {
             INTENSIVGA QO'SHILISH <ArrowLeft className="inline w-12" />
           </button>
         </div>
+       <footer className="w-full py-6">
+          <div className="flex items-center justify-center gap-3">
+            <p className="text-gray-500 text-sm">Created by</p>
+            <a
+              href="https://t.me/it_zoneuz"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:opacity-80 transition-all duration-300 transform hover:scale-105"
+            >
+              <Image
+                src="/itzone.png"
+                alt="IT Zone Telegram"
+                width={150}
+                height={100}
+                loading="lazy"
+                className="cursor-pointer filter brightness-12 invert-[20%] sepia-[300%] saturate-[10000%] hue-rotate-[500deg]"
+              />
+            </a>
+          </div>
+        </footer>
 
         {/* Footer */}
         
